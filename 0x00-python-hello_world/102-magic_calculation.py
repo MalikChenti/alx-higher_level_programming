37c4f30afed0897b44f539b9d155f17dec7be0e5
MalikChenti <malik.abu6889@gmail.com>
2023-01-05 06:14:47 -0800

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-return (98 + a ** b)
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+         str = str[39:66] + str[106:112] + str[:6]
+         print(str)
