720b7a4642364ea87969081edec6cd241e883f32
MalikChenti <malik.abu6889@gmail.com>
2023-01-05 14:10:05 -0800

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    ld = number % -10
-else:
-    ld = number % 10
+lastDigit = 0
 
-    if ld > 5:
-        print(f"Last digit of {number} is {ld:d} and is greater than 5")
-    elif ld == 0:
-        print(f"Last digit of {number} is {ld:d} and is 0")
+if number >= 0:
+        lastDigit = number % 10
     else:
-        print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
+            lastDigit = (-number % 10) * -1
+
+            message = f"Last digit of {number} is {lastDigit}"
+
+            if lastDigit == 0:
+                    print(f"{message} and is 0")
+                elif lastDigit > 5 and lastDigit % 10 != 0:
+                        print(f"{message} and is greater than 5")
+                    else:
+                            print(f"{message} and is less than 6 and not 0")
